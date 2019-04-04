62375b9cce998423d36c1b5dd21d56cae0f541bf
@Gasimov <aziz.gasimov94@gmail.com>
2019-04-04 08:40:37 +0200

    UserServiceImpl class implemented. it implements UserService interface

---
+++ src/main/java/com/userFront/service/UserServiceImpl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.userFront.service.UserServiceImpl;
+
+import com.userFront.Dao.UserDao;
+import com.userFront.domain.User;
+import com.userFront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public void seva(User user){
+        userDao.save(user);
+    }
+
+    public User findByUsername(String username){
+        return userDao.findByUsername(username);
+    }
+
+    public User findByEmail(String email){
+        return userDao.findByEmail(email);
+    }
+    public boolean checkUserExists(String username, String email){
+        if(checkUsernameExists(username)|| checkEmailExists(email)){
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+    public boolean checkUsernameExists(String username){
+        if(findByUsername(username) != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public boolean checkEmailExists(String email){
+        if(findByEmail(email) != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
